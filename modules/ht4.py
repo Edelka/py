12d1d401eb61eeb2b145abcbfc3c6602365fbad9
Edelka <33692062+Edelka@users.noreply.github.com>
2023-10-20 19:34:54 +0300

    Add files via upload

---
+++ modules/ht4.py
@@ -0,0 +1,55 @@
+import re
+
+text = """homEwork:
+	tHis iz your homeWork, copy these Text to variable. 
+
+	You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+	it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE. 
+
+	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
+"""
+
+
+# Task: to count number of spaces
+def counts(some_texts):
+    count = 0  # initialize counter value to zero
+    for i in some_texts:  # incrementing counter whenever isspace() returns True
+        if i.isspace():
+            count = count + 1
+    return count
+
+
+# print("The number of spaces is: ", counts(text))
+
+
+def n_string(some_string):
+    new_string = ''  # initialize new string
+    # remove spaces and capitalize 1st letter and other letters move to lower case
+    repl_text = text.strip().capitalize().replace(' iz ', ' is ').split('.')
+    for m in repl_text:  # iterating elements in list
+        for j in m:  # iterating elements in lists elements
+
+            # print(j)
+            if j.isspace():  # find spaces
+                new_string = new_string + j  # move spaces to new string
+            else:
+                new_string = new_string + j.capitalize()  # capitalize 1st letter of the sentence
+                new_string = new_string + m[m.find(j) + 1:]  # move the rest of sentence to new string
+                new_string = new_string + m[-1].replace(m[-1], '.')  # set a dot at the end of the sentence
+                break
+    res = "{}".format(new_string)
+    return res
+# print(n_string(text))
+
+def sent(some_sent):
+    sentence = ' '.join(re.findall(r'\S*[.]', n_string(text))).replace('.', '').capitalize()
+    # print(sentence)
+
+    # join formatted text and new sentence
+    result = n_string(text) + '' + sentence + '.'
+
+    return result
+
+
+print(sent(text))
---
+++ modules/th6a.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import os
+from ht4 import n_string as normal_text
+
+PATH = r'C:/Users/Olena_Pakholchak/Desktop/New folder (2)/newsfeed.txt'
+
+# path = 'C:/Users/Olena_Pakholchak/PycharmProjects/pnew1/modules/newsfeed.txt'
+
+
+# Create a function that will write new publications to the file
+# def to_file():
+#     try:
+#         f = open(path, 'a')
+#         f.write(p + '\n\n\n')
+#         f.close()
+#     finally:
+#         print('If you want please repeat again with new topics.')
+
+# n = News('some text','some city2')
+# n.write_data()
+
+
+# write_data()
+
+
+# print(write_data())
+
+
+# def publ():
+#     while True:
+#         m = input('Type Yes, if you want to continue: ')
+#         if m == 'Yes':
+#             publish()
+#             to_file()
+#         else:
+#             print('Thanks!')
+#             break
+#         break
+#
+#
+# while True:
+#     publ()
